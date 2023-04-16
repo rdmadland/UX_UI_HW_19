ad07bd9690f570683c6ad5154039e6f48c2abc4d
rdmadland <rdmadland@gmail.com>
2023-04-16 15:48:21 -0700

    Card animation on hover
    Updated to do animation on hover.  Using scroll was presenting problems.

--- js/index.js
+++ js/index.js
@@ -1,39 +1,23 @@
+$('.work-item').hover(
+    function() {
+        $(this).find('div').animate({
+            right: '0%'
+        }, 'slow' );
+    },function() {
+        $(this).find('div').animate({
+            right: '-100%'
+        },'slow');
+    }
+);
 
-console.log("Your index.js file is loaded correctly!");
-
-
-const sectionOneOptions = document.querySelectorAll('fade-in');
-
-const appearOptions = {
-    threshold: 1
-    
-   
-}
-
-const faders = document.querySelectorAll("fade-in")
-
-const sliders = document.querySelectorAll("sliders")
-
-const appearOnScroll = new IntersectionObserver(function(
-    entries,
-    appearOnScroll
-
-) {
-    entries.forEach(entry => {
-        if(!entry.isIntersecting) {
-            return;
-        } else {
-            entry.target.classList.add('appear');
-            appearOnScroll.unobserve(entry.target);
-        }
-    })
-appearOptions});
-
-
-faders.forEach(faders => {
-    appearOnScroll.observe(faders);
-})
-
-sliders.forEach(sliders => {
-    appearOnScroll.observe(sliders)
-})
+$('.work-item-right').hover(
+    function() {
+        $(this).find('div').animate({
+            right: '0%'
+        }, 'slow' );
+    },function() {
+        $(this).find('div').animate({
+            right: '-100%'
+        },'slow');
+    }
+);
