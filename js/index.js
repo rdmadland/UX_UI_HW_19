5ac21962899f9ea3bfaacd007a583da0ae71b134
rdmadland <rdmadland@gmail.com>
2023-04-18 14:15:54 -0700

    Added emergence animation
    Updated card animations using emergence.  Cleaned up classes for cards no longer used.

--- js/index.js
+++ js/index.js
@@ -1,25 +1,26 @@
 
-$('.work-item-right').hover(
-    function() {
-        $(this).find('div').animate({
-            right: '0%'
-        }, 'slow' );
-    },function() {
-        $(this).find('div').animate({
-            right: '-100%'
-        },'slow');
-    }
-);
+// $('.work-item-right').hover(
+//     function() {
+//         $(this).find('div').animate({
+//             right: '0%'
+//         }, 'slow' );
+//     },function() {
+//         $(this).find('div').animate({
+//             right: '-100%'
+//         },'slow');
+//     }
+// );
 
 
-$('.work-item').hover(
-    function() {
-        $(this).find('div').animate({
-            right: '0%'
-        }, 'slow' );
-    },function() {
-        $(this).find('div').animate({
-            right: '-100%'
-        },'slow');
-    }
-);
+// $('.work-item').hover(
+//     function() {
+//         $(this).find('div').animate({
+//             right: '0%'
+//         }, 'slow' );
+//     },function() {
+//         $(this).find('div').animate({
+//             right: '-100%'
+//         },'slow');
+//     }
+// );
+
